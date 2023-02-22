4b56984a2393aacdc957627112be06a121909f64
jantieku <jantieku@gmail.com>
2023-02-22 07:46:10 +0300

    first commit

---
+++ capturingUserData/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from .forms import UserForm
+
+def index(request):
+    submit_button = request.POST.get("submit")
+    name = ''
+    email = ''
+    password = ''
+
+    form = UserForm(request.POST or None)
+    if form.is_valid():
+        name = form.cleaned_data.get("name")
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+    context = {'form': form, 'name': name, 'email': email, 'password': password, 'submit_button': submit_button}
+
+
+    return render(request, 'index.html', context)
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def register(request):
+    return render(request, 'register.html')
